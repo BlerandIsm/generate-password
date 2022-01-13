9dc2a027d440993549c57639bf8f221979548c47
Blerand Ismaili <94268979+BlerandIsm@users.noreply.github.com>
2022-01-12 20:51:45 -0500

    adding the user interaction promt

--- Develop/script.js
+++ Develop/script.js
@@ -7,10 +7,47 @@ var generateBtn = document.querySelector("#generate");
 //create the user input 
 function getPasswordOptions(userNumCharacers){
   if (isNaN(userNumCharacers)){
-    alert("Please enter a valid number from 8-128 !");
+    alert("Please enter a valid number !");
+    return false;
+  } else if(parseInt(userNumCharacers) < 8){
+    alert("Password must be at least 8 characters !");
+    return false;
+  } else if(parseInt(userNumCharacers) >= 128){
+    alert("Password must be less then 128 characters");
     return false;
   }
+  return true;
 }
+// get random characters from each chosen character type
+function getRandomElementFromArray(collection){
+  return collection[Math.floor(Math.random())* collection.length];
+}
+
+//bulding the user input with promt functions 
+function generatePassword(){
+  var userNumCharacers= prompt(
+    "How many character do you want in your password ?"
+  );
+  var passwordValid = getPasswordOptions(userNumCharacers);
+  if (passwordValid){
+    var hasSpecialCharacters = confirm(
+      "Click OK to confirm special characters."
+    );
+    var hasNumbers= confirm("Click OK to confirm adding numbers.");
+    var hasLowerCase= confirm("Click OK to confirm adding lower case characters");
+    var hasUperCase=confirm("Click OK to confirm adding upper case characters ");
+  }
+// arrays to store typer of characters
+  var chosenChar=[];
+  var guaranteeedChar=[];
+  
+  
+
+ 
+}
+
+
+
 //arrey of special characters
 var specialCharacter=[
   ",",
@@ -107,47 +144,9 @@ var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 var passwordLength = 
 
-function generatePassword(){
-  //ask for user input
 
-
-
-
-
-
-
-
-
-  return " pasword will go here";
-}
 
 // Write password to the #password input
 function writePassword() {
